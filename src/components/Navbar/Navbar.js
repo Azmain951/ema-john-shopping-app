7bad3769c95d5c4b1a0fb434aae47503565f836b
Afridi Azmain <Azmain951>
2022-03-24 13:27:50 +0600

    favicon,title and Navbar component added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className='navbar'>
+                <a href='/home'><img src={logo} alt="" /></a>
+                <div className='menu-items'>
+                    <a href="/shop">Shop</a>
+                    <a href="/orders">Orders</a>
+                    <a href="/inventory">Inventory</a>
+                    <a href="/about">About</a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
